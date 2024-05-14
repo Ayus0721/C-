d665617dcad61f07ae6270f709ef8fd8f7ca7f1d
AYUSH <147092631+Ayus0721@users.noreply.github.com>
2024-05-15 00:28:25 +0530

    Update and rename practical9.cpp to person_mem.cpp

--- person_mem.cpp
+++ person_mem.cpp
@@ -1,4 +1,6 @@
-// Define a class Person having name as a data member. Inherit two classes Student and Employee from Person. Student has additional attributes as course, marks and year and Employee has department and salary. Write display() method in all the three classes to display the corresponding attributes. Provide the necessary methods to show runtime polymorphism.
+// Define a class Person having name as a data member. Inherit two classes Student and Employee from Person. 
+// Student has additional attributes as course, marks and year and Employee has department and salary. Write display() method in all the three classes to display the corresponding attributes. 
+// Provide the necessary methods to show runtime polymorphism.
 
 #include <iostream>
 #include <string>
