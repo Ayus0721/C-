83f43a7f1c4b757bae8b4af8dd4bc68c3c8c9e9b
AYUSH <147092631+Ayus0721@users.noreply.github.com>
2024-05-15 00:26:47 +0530

    Update and rename practical8.cpp to matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,6 @@
-/*Create a Matrix class. Write a menu-driven program to perform following Matrix
-operations (exceptions should be thrown by the functions if matrices passed to them are
-incompatible and handled by the main() function):*/
+//*Create a Matrix class. Write a menu-driven program to perform following Matrix
+//operations (exceptions should be thrown by the functions if matrices passed to them are
+//incompatible and handled by the main() function):*/
 // a. Sum
 // b. Product
 // c. Transpose
